0d18bc5805dd3ba3bb9c8f13527fa85a4ae8c3c4
maryd-k <maryia.dyliuk@gmail.com>
2021-02-23 13:37:52 +0300

    fear: add matrix sort function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,32 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (matrix === undefined || matrix.length === 0)
+    {
+        let emptyMas = [];
+        return emptyMas;
+    }
+    else
+    {
+    let newMas = [];
+    let kol = 0;
+    for (let i=0; i<matrix.length; i++ )
+    {
+        if ( i % 2 == 0)
+        {
+        for (let j=0; j<matrix[i].length; j++, kol++)
+        {
+            newMas[kol]=matrix[i][j];
+        }
+      }
+      else 
+      {
+          for ( let f =(matrix[i].length)-1; f>=0; f--, kol++)
+          {
+              newMas[kol]=matrix[i][f];
+          }
+      }
+    }
+    return newMas;
+}
 }
